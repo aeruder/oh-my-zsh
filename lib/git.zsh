922188551f10ec7e510a20d40b1f626cca12df83
Andrew Ruder <andrew.ruder@elecsyscorp.com>
2013-08-21 03:18:01 -0500

    dirty/clean flag takes too much time in oh-my-zsh

--- lib/git.zsh
+++ lib/git.zsh
@@ -6,11 +6,7 @@ function git_prompt_info() {
 
 # Checks if working tree is dirty
 parse_git_dirty() {
-  if [[ -n $(git status -s 2> /dev/null) ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  else
     echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-  fi
 }
 
 # Checks if there are commits ahead from remote
